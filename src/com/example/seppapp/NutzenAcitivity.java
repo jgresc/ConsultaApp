310540b73db106e823ef1b2169cb96ace65eea38
Jerinas <jerinas.gresch@gmail.com>
2018-09-29 13:41:43 +0200

    inital commit

---
+++ src/com/example/seppapp/NutzenAcitivity.java
@@ -0,0 +1,52 @@
+package com.example.seppapp;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class NutzenAcitivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nutzen_acitivity);
+        
+        final Button analyseBtn = (Button) findViewById(R.id.analyse);
+        analyseBtn.setOnClickListener(new View.OnClickListener(){
+        	
+        	public void onClick(View v){
+        		
+        		startActivity(new Intent("TableActivity"));
+        		
+        		
+        	}
+        });
+        
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.nutzen_acitivity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+}
